1274c85448bdd57d853228fe25c3c3f7e5a64a8a
QubitPi <jack20220723@gmail.com>
2024-01-10 14:45:23 +0000

    deploy: 66f518c157d621b4f37ad0ac9b77980cb1794086

---
+++ assets/js/33f5a029.64b4b595.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[3974],{8406:e=>{e.exports=JSON.parse('{"label":"Apache Avro","permalink":"/jersey-webservice-template/blog/tags/apache-avro","allTagsPath":"/jersey-webservice-template/blog/tags","count":1,"unlisted":false}')}}]);
