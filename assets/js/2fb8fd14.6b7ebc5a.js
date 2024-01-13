14df5a354f7734839483e8c0a5e874a41811d93d
QubitPi <jack20220723@gmail.com>
2024-01-13 02:40:20 +0000

    deploy: 9ffebe2b495e2b88ab7e0793417ae57707ea80c2

---
+++ assets/js/2fb8fd14.6b7ebc5a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[2447],{3463:e=>{e.exports=JSON.parse('{"label":"JPA","permalink":"/jersey-webservice-template/blog/tags/jpa","allTagsPath":"/jersey-webservice-template/blog/tags","count":4,"unlisted":false}')}}]);
---
+++ assets/js/b0c4e38f.fedab5ad.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[4825],{2489:e=>{e.exports=JSON.parse('{"label":"Database","permalink":"/jersey-webservice-template/blog/tags/database","allTagsPath":"/jersey-webservice-template/blog/tags","count":4,"unlisted":false}')}}]);
